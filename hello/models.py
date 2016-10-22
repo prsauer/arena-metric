48a9c54af27abe97e4b8ab9b7d13a311d30c379f
Paul Sauer <banthur@gmail.com>
2016-10-22 15:42:13 -0400

    gdi more pepes

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Pepe(models.Model):
+    command = models.CharField(null=False, unique=True, max_length=32)
+    url = models.CharField(null=False, unique=True, max_length=512)
+
+from django.contrib import admin
+
+admin.site.register(Pepe)
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from .models import Pepe
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -12,8 +14,22 @@ def db(request):
     return render(request, 'db.html', {})
 
 def pepe(request):
-    r = {"response_type": "in_channel"}
-    im = "http://i3.kym-cdn.com/photos/images/facebook/000/862/065/0e9.jpg"
-    attachments = [{"image_url": im},]
-    r['attachments'] = attachments
-    return JsonResponse(r)
+    if request.GET.get('text') and request.GET.get('text') == 'help':
+        tm = "<table><thead><td>Command</td><td>Pepe</td></thead><tbody>"
+        for p in Pepe.objects.all():
+            tr = "<tr><td>%s</td><td><img src=\"%s\"></img></td></tr>"%(p.command, p.url)
+            tm+=tr
+        tm+="</tbody></table>"
+        return HttpResponse(tm)
+    try:
+        r = {"response_type": "in_channel"}
+        im = Pepe.objects.get(command=request.GET.get('text')).url
+        attachments = [{"image_url": im},]
+        r['attachments'] = attachments
+        return JsonResponse(r)
+    except:
+        r = {"response_type": "in_channel"}
+        im = "http://i3.kym-cdn.com/photos/images/facebook/000/862/065/0e9.jpg"
+        attachments = [{"image_url": im},]
+        r['attachments'] = attachments
+        return JsonResponse(r)
